d932330bcab845a33a9f0ddcdd1fd4b22001d92d
Shaked <shvshv44@gmail.com>
2025-09-22 18:02:17 +0300

    handle icons and improve daily box

--- Scripts/UI/calendar_day_box.gd
+++ Scripts/UI/calendar_day_box.gd
@@ -2,9 +2,6 @@
 extends Control
 class_name CalendarDayBox
 
-# TODO: need to load those dynamically by activity type.
-var test_icon_texture: Texture2D = preload("res://Assets/volleyball_8673941.png")
-
 @export var day_num: int = 0:
 	set = set_day_num
 @export var activity_type: DailyActivityType.Type = DailyActivityType.Type.Break:
@@ -19,22 +16,45 @@ var test_icon_texture: Texture2D = preload("res://Assets/volleyball_8673941.png"
 
 func _ready():
 	box_border.color = ColorUtils.DARK_UI_MAIN_COLOR
+	# Safe initialization after nodes exist
+	update_activity_icon()
+	update_day_label()
+	update_current_day_color()
 
-func set_day_num(_day: int):
+# --- Setters ---
+func set_day_num(_day: int) -> void:
 	day_num = _day
-	day_label.text = str(_day)
-	
-func set_activity(_type: DailyActivityType.Type):
+	update_day_label()
+
+func set_activity(_type: DailyActivityType.Type) -> void:
 	activity_type = _type
+	update_activity_icon()
+
+func set_is_current_day(_is_current_day: bool) -> void:
+	is_current_day = _is_current_day
+	update_current_day_color()
+
+# --- Internal update methods ---
+func update_day_label() -> void:
+	if day_label:
+		day_label.text = str(day_num)
+
+func update_activity_icon() -> void:
+	if not activity_icon:
+		return # Node not ready yet
 	if activity_type == DailyActivityType.Type.Break:
 		activity_icon.hide()
 	else:
-		# TODO: put based on the activity
-		activity_icon.texture = test_icon_texture
+		# Safe editor/runtime assignment
+		if Icons.ACTIVITY_ICON_MAP.has(activity_type):
+			activity_icon.texture = Icons.ACTIVITY_ICON_MAP[activity_type]
+		else:
+			activity_icon.texture = null
 		activity_icon.show()
 
-func set_is_current_day(_is_current_day: bool):
-	is_current_day = _is_current_day
+func update_current_day_color() -> void:
+	if not box_space_fill:
+		return
 	if is_current_day:
 		box_space_fill.color = ColorUtils.BRIGHT_UI_PICK_COLOR
 	else:
